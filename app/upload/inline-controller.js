d3f979fab0a67de24fd492a897a68b38183597d8
Juntao Wu <wujuntaocn@live.cn>
2016-08-11 23:25:21 +0800

    comment unneccesary code.

--- app/upload/inline-controller.js
+++ app/upload/inline-controller.js
@@ -22,43 +22,43 @@ angular.module('myApp.upload', ['ngRoute', 'angularFileUpload'])
         
         $scope.currentFile = "";
         
-        uploader.onWhenAddingFileFailed = function (item /*{File|FileLikeObject}*/, filter, options) {
-            console.info('onWhenAddingFileFailed', item, filter, options);
-        };
+        //uploader.onWhenAddingFileFailed = function (item /*{File|FileLikeObject}*/, filter, options) {
+        //    console.info('onWhenAddingFileFailed', item, filter, options);
+        //};
         uploader.onAfterAddingFile = function (fileItem) {
             console.info('onAfterAddingFile', fileItem);
             $scope.currentFile = fileItem.name;
         };
-        uploader.onAfterAddingAll = function (addedFileItems) {
-            console.info('onAfterAddingAll', addedFileItems);
-        };
+        //uploader.onAfterAddingAll = function (addedFileItems) {
+        //    console.info('onAfterAddingAll', addedFileItems);
+        //};
         uploader.onBeforeUploadItem = function (item) {
             console.info('onBeforeUploadItem', item);
             item.formData.push({ token: $scope.token });
             item.formData.push({ key: item.file.name });
         };
-        uploader.onProgressItem = function (fileItem, progress) {
-            console.info('onProgressItem', fileItem, progress);
-        };
-        uploader.onProgressAll = function (progress) {
-            console.info('onProgressAll', progress);
-        };
-        uploader.onSuccessItem = function (fileItem, response, status, headers) {
-            console.info('onSuccessItem', fileItem, response, status, headers);
-        };
-        uploader.onErrorItem = function (fileItem, response, status, headers) {
-            console.info('onErrorItem', fileItem, response, status, headers);
-        };
-        uploader.onCancelItem = function (fileItem, response, status, headers) {
-            console.info('onCancelItem', fileItem, response, status, headers);
-        };
+        //uploader.onProgressItem = function (fileItem, progress) {
+        //    console.info('onProgressItem', fileItem, progress);
+        //};
+        //uploader.onProgressAll = function (progress) {
+        //    console.info('onProgressAll', progress);
+        //};
+        //uploader.onSuccessItem = function (fileItem, response, status, headers) {
+        //    console.info('onSuccessItem', fileItem, response, status, headers);
+        //};
+        //uploader.onErrorItem = function (fileItem, response, status, headers) {
+        //    console.info('onErrorItem', fileItem, response, status, headers);
+        //};
+        //uploader.onCancelItem = function (fileItem, response, status, headers) {
+        //    console.info('onCancelItem', fileItem, response, status, headers);
+        //};
         uploader.onCompleteItem = function (fileItem, response, status, headers) {
             console.info('onCompleteItem', fileItem, response, status, headers);
             fileItem.url = "http://7xsvf8.com1.z0.glb.clouddn.com/" + response.key;
         };
-        uploader.onCompleteAll = function () {
-            console.info('onCompleteAll');
-        };
+        //uploader.onCompleteAll = function () {
+        //    console.info('onCompleteAll');
+        //};
 
 
     }]);
