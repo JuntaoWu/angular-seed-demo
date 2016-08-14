c0de249ec94520dcee8310a3be4722891a42a1bd
Juntao Wu <wujuntaocn@live.cn>
2016-08-14 20:53:21 +0800

    bower install ng-repeat-n

--- app/app.js
+++ app/app.js
@@ -8,7 +8,8 @@ angular.module('myApp', [
     'myApp.upload',
     'myApp.nav',
     'myApp.version',
-    'myApp.common'
+    'myApp.common',
+    'angular-repeat-n'
 ]).
 config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
         $locationProvider.hashPrefix('!');
